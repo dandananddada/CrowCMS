f43c434cd24db64f8236bd802c2dd14072402517
Eten Gstar <dandananddada@163.com>
2015-03-04 22:33:29 +0800

    删除rails原生测试类

--- spec/requests/login_tests_spec.rb
+++ spec/requests/login_tests_spec.rb
@@ -6,14 +6,21 @@ RSpec.describe "LoginTests", type: :request do
   		@user = create(:user, password: "admin1234")
   	end
 
-
-    it "works! (now write some real specs)" do
+    it "correct login session" do
       # user = build(:user)
       visit new_user_session_path
       expect(page).to have_content("后台管理")
       fill_in 'user_password', with: "admin1234"
       page.find('.btn-primary').click
       expect(page).to have_content("首页"), "登陆失败"
+    end
+
+    it "error login session" do
+      visit new_user_session_path
+      expect(page).to have_content("后台管理")
+      fill_in 'user_password', with: "admin"
+      page.find('.btn-primary').click
+      expect(page).to have_content("后台管理")
     end
   end
 end
--- test/models/admin/article_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Admin::ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
