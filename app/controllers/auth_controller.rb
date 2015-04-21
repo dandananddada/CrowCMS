bb34dd1c8a8d2a877ae091b555259fc7e642ee6d
Eten Gstar <dandananddada@163.com>
2015-04-21 13:10:07 +0800

    fix bug about statistic chart can not update.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,6 +3,6 @@ class AuthController < ApplicationController
  	# For APIs, you may want to use :null_session instead.
  	
  	protect_from_forgery with: :exception
- 	before_action :authenticate_user!&&current_user.admin?
+ 	before_action :authenticate_user!
 
 end
